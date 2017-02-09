83fa7b4161736e4d018e4969d15645d7d7a1014b
OniiChan <shanukmr197@gmail.com>
2017-02-10 03:09:43 +0530

    Added Recycler View and Implemented new Dependencies and also added a ToolBar

--- app/src/main/java/com/oniichan/attendanceapp/activities/MainActivity.java
+++ app/src/main/java/com/oniichan/attendanceapp/activities/MainActivity.java
@@ -1,16 +1,38 @@
 package com.oniichan.attendanceapp.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.MenuItemHoverListener;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.oniichan.attendanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        mToolbar.setSubtitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        return true;
     }
 }
